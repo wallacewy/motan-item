44b5c1e77805077fe5eecded3d0eb04b8206d31f
Wallace Wan <wallace.wan@sap.com>
2016-11-04 10:12:48 +0800

    Initial project

---
+++ src/main/java/com/wallace/motan/ItemServiceImpl.java
@@ -0,0 +1,9 @@
+package com.wallace.motan;
+
+public class ItemServiceImpl implements ItemService {
+	
+	public String hello(String name) {
+        System.out.println(name + " invoked item service");
+        return "Hello " + name + ", it's greeting from item service";
+    }
+}
